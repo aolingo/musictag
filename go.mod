10cfe3e72ff5bd23b2535d845fba2a5e21f2e8a4
Ao Lin <gongzhilin@hotmail.com>
2020-04-29 19:27:31 -0700

    added skeleton code to decode mp3 file metadata for song name

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/aolingo/musictag
 
 go 1.13
+
+require (
+	github.com/dhowden/tag v0.0.0-20200412032933-5d76b8eaae27
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da // indirect
+	github.com/mikkyang/id3-go v0.0.0-20191012064224-2c6ab3bb1fbd
+)
--- main.go
+++ main.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/dhowden/tag"
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	// First element in os.Args is always the program name,
+	// 2nd argument = music directory path
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file directory")
+		return
 	}
-	fmt.Println(s)
+
+	fmt.Println("Input file dir is " + os.Args[1])
+	files, err := ioutil.ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		p := filepath.Join(os.Args[1], f.Name())
+		file, err := os.Open(p)
+		if err != nil {
+			fmt.Println("Can't open file:", f.Name())
+			panic(err)
+		}
+
+		m, err := tag.ReadFrom(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("song name: ", m.Title())
+	}
+
 }
