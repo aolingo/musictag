a5403200174966c0d5fd3901fed460f0b6de6e42
Ao Lin <gongzhilin@hotmail.com>
2020-05-01 15:11:35 -0700

    refactor code from main func to helpers

--- musictag.go
+++ musictag.go
@@ -9,42 +9,15 @@ import (
 	"github.com/dhowden/tag"
 )
 
-func main() {
-	// First element in os.Args is always the program name,
-	// 2nd argument = name of the iPod music directory
-	if len(os.Args) < 2 {
-		fmt.Println("Missing parameter, provide file directory")
-		return
-	}
+var logFile *os.File
+var files []string
 
-	fmt.Println("Input dir is " + os.Args[1])
-
-	var files []string
-
-	// recursively scan for all songs contained in all the F subdirectories
-	err := filepath.Walk(os.Args[1],
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.IsDir() {
-				files = append(files, path)
-			}
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
-
+// renameSongs will replace the name of all mp3 files with
+// the song title in its ID3 metadata tag, if it has one
+func renameSongs() {
 	fmt.Printf("%d songs detected, beginning renaming\n", len(files))
 
-	// Initialize a log file for tracking all renamed songs and their names
-	logFile, err := os.Create("results.txt")
-	if err != nil {
-		fmt.Println(err)
-		logFile.Close()
-		return
-	}
+	renameCount := 0
 
 	for _, f := range files {
 		file, err := os.Open(f)
@@ -58,13 +31,72 @@ func main() {
 			log.Fatal(err)
 		}
 
-		logStr := file.Name() + " -> " + m.Title()
-		fmt.Fprintln(logFile, logStr)
-		fmt.Println("song name: ", m.Title())
+		// rename file if a title can be found from the file's ID3 tag
+		if m.Title() != "" {
+			// log the name conversion for reference
+			logStr := file.Name() + " -> " + m.Title()
+			fmt.Fprintln(logFile, logStr)
+			// close and rename the file
+			file.Close()
+
+			// dir := filepath.Dir(f)
+			base := filepath.Base(f)
+			fmt.Println(base)
+
+			// fmt.Printf("mv %q %q\n", file.Name(), newpath)
+			// err := os.Rename(file.Name(), m.Title())
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			renameCount++
+		}
 	}
 
+	fmt.Printf("Successfully renamed %d files", renameCount)
+
+}
+
+func visit(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		files = append(files, path)
+	}
+
+	return nil
+}
+
+func main() {
+	// First element in os.Args is always the program name,
+	// 2nd argument = name of the iPod music directory
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file directory")
+		return
+	}
+
+	fmt.Println("Input dir is " + os.Args[1])
+
+	// Initialize a log file for tracking all renamed songs and their names
+	var logErr error
+	logFile, logErr = os.Create("results.txt")
+	if logErr != nil {
+		fmt.Println(logErr)
+		logFile.Close()
+		return
+	}
+	fmt.Fprintln(logFile, "old name -> new name")
+
+	// recursively scan for all songs contained in all the F subdirectories
+	walkErr := filepath.Walk(os.Args[1], visit)
+	if walkErr != nil {
+		log.Println(walkErr)
+	}
+
+	renameSongs()
+
 	// Close log file
-	err = logFile.Close()
+	err := logFile.Close()
 	if err != nil {
 		fmt.Println(err)
 		return
