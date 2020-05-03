7072aef6fe372978b6f45f37c89324e9c6f7b7f7
Ao Lin <gongzhilin@hotmail.com>
2020-05-02 18:56:00 -0700

    all functionalities implemented and added progess bar for cli

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.com/aolingo/musictag
 go 1.13
 
 require (
+	github.com/cheggaaa/pb v2.0.7+incompatible
+	github.com/cheggaaa/pb/v3 v3.0.4 // indirect
 	github.com/dhowden/tag v0.0.0-20200412032933-5d76b8eaae27
 	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da // indirect
 	github.com/mikkyang/id3-go v0.0.0-20191012064224-2c6ab3bb1fbd
+	gopkg.in/VividCortex/ewma.v1 v1.1.1 // indirect
+	gopkg.in/cheggaaa/pb.v2 v2.0.7 // indirect
+	gopkg.in/fatih/color.v1 v1.7.0 // indirect
+	gopkg.in/mattn/go-colorable.v0 v0.1.0 // indirect
+	gopkg.in/mattn/go-isatty.v0 v0.0.4 // indirect
+	gopkg.in/mattn/go-runewidth.v0 v0.0.4 // indirect
 )
--- musictag.go
+++ musictag.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/cheggaaa/pb"
 	"github.com/dhowden/tag"
 )
 
@@ -18,14 +21,27 @@ func renameSongs() {
 	fmt.Printf("%d songs detected, beginning renaming\n", len(files))
 
 	renameCount := 0
+	// create and start a new progress bar
+	bar := pb.StartNew(len(files))
 
 	for _, f := range files {
+		bar.Increment()
+		time.Sleep(time.Millisecond * 10)
+
+		// extra safeguard to skip a mp3 file if it's file name is already decoded
+		// eg. it doesn't have the form ABCD, where ABCD are 4 uppercase letters
+		oldname := strings.Replace(filepath.Base(f), ".mp3", "", 1)
+		if len(oldname) != 4 || strings.ToUpper(oldname) != oldname {
+			continue
+		}
+
 		file, err := os.Open(f)
 		if err != nil {
 			fmt.Println("Can't open file:", f)
 			panic(err)
 		}
 
+		// get the ID3 tag from the opened mp3 file
 		m, err := tag.ReadFrom(file)
 		if err != nil {
 			log.Fatal(err)
@@ -33,37 +49,49 @@ func renameSongs() {
 
 		// rename file if a title can be found from the file's ID3 tag
 		if m.Title() != "" {
+			// retrive song title from tag and do some cleaning before renaming
+			newbase := strings.TrimSpace(m.Title())
+			if !strings.Contains(newbase, ".mp3") {
+				newbase += ".mp3"
+			}
+			newpath := filepath.Join(filepath.Dir(f), newbase)
+
 			// log the name conversion for reference
-			logStr := file.Name() + " -> " + m.Title()
+			logStr := file.Name() + " -> " + newbase
 			fmt.Fprintln(logFile, logStr)
+
 			// close and rename the file
 			file.Close()
 
-			// dir := filepath.Dir(f)
-			base := filepath.Base(f)
-			fmt.Println(base)
-
-			// fmt.Printf("mv %q %q\n", file.Name(), newpath)
-			// err := os.Rename(file.Name(), m.Title())
-			// if err != nil {
-			// 	log.Fatal(err)
-			// }
+			err := os.Rename(file.Name(), newpath)
+			if err != nil {
+				log.Fatal(err)
+			}
 			renameCount++
 		}
 	}
 
-	fmt.Printf("Successfully renamed %d files", renameCount)
+	bar.Finish()
 
+	if renameCount == len(files) {
+		fmt.Printf("Succesfully renamed all detected songs, results are logged in results.txt\n")
+	} else {
+		skipped := len(files) - renameCount
+		fmt.Printf("Successfully renamed %d songs, skipped %d files due to incomplete info in file's metadata\n", renameCount, skipped)
+		fmt.Println("File renaming results are logged in results.txt")
+	}
 }
 
+// visit is the helper walkFn called by filepath.Walk,
+// it tracks all the .mp3 files found in the input
+// directory and its subdirectories
 func visit(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		return err
 	}
-	if !info.IsDir() {
+	if !info.IsDir() && filepath.Ext(path) == ".mp3" {
 		files = append(files, path)
 	}
-
 	return nil
 }
 
@@ -93,6 +121,9 @@ func main() {
 		log.Println(walkErr)
 	}
 
+	// Can optimize the program's time complexity from O(2n) to O(n) in the future
+	// by implementing renameSongs() in the visit walkFn helper (scan and rename
+	// at the same time instead of rename after scanning all the files)
 	renameSongs()
 
 	// Close log file
